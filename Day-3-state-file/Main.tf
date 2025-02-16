e9992a4fd4da730aa5d5a3f8cfd6612d9df73ed6
karthikvodapelly2001 <karthikvodapelly2001@gmail.com>
2025-02-16 17:06:12 +0530

    modified the main.tf branch

--- Day-3-state-file/Main.tf
+++ Day-3-state-file/Main.tf
@@ -1,7 +1,10 @@
 resource "aws_instance" "name" {
-  ami = "ami-085ad6ae776d8f09c"
-  key_name = "nv-personal-KP"
+  ami           = "ami-085ad6ae776d8f09c"
+  key_name      = "nv-personal-KP"
   instance_type = "t2.micro"
 
-
+  tags = {
+    Name = "My-Server"  # This sets the instance name
+    Environment = "Dev"  # You can add more tags if needed
+  }
 }
