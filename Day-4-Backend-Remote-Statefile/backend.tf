b88c8e6789b1abea3a94db1e9f50b47dcde9ef76
karthikvodapelly2001 <karthikvodapelly2001@gmail.com>
2025-02-18 23:24:24 +0530

    commit

--- Day-4-Backend-Remote-Statefile/backend.tf
+++ Day-4-Backend-Remote-Statefile/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "mybuckettmumbaii"
+    bucket         = "buckettdynamodb"
     key            = "terraform.tfstate"
-    region = "ap-south-1"
+    region         = "us-east-1"  # Make sure this matches DynamoDB region
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
+
--- Day-4-DynamoDB/main.tf
+++ Day-4-DynamoDB/main.tf
@@ -8,8 +8,8 @@ resource "aws_dynamodb_table" "terraform-locks" {
   
   name         = "terraform-locks"
 #   billing_mode = "PAY_PER_REQUEST"
-read_capacity = 3
-write_capacity = 3
+read_capacity = 20
+write_capacity = 20
 
   hash_key     = "LockID"
 
