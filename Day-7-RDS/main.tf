db7fddfbcb8ee1364177311c962ec86ff34e82d1
karthikvodapelly2001 <karthikvodapelly2001@gmail.com>
2025-02-20 13:13:26 +0530

    RDS creation using terraform

---
+++ Day-7-RDS/main.tf
@@ -0,0 +1,24 @@
+resource "aws_db_instance" "prodRDS" {
+  allocated_storage    = 20
+  identifier = "prod-rds"
+  db_name              = "prodrds"
+  engine               = "mysql"
+  engine_version       = "8.0.40"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "Karthik123"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  availability_zone = "us-east-1b"
+  db_subnet_group_name = "tf-subnetgroup"
+
+
+  tags = {
+    Name="tfproddb"
+  }
+}
+
+
+output "RDS" {
+  value = aws_db_instance.prodRDS.engine
+}
